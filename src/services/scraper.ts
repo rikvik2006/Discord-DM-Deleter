1e36f78c28521afcbf1a7f57948543a1dd997a15
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2023-09-19 23:40:35 +0200

    feat: Created the scraper handler

---
+++ src/services/scraper.ts
@@ -0,0 +1,31 @@
+import puppeteer from "puppeteer-extra"
+import StealthPlugin from "puppeteer-extra-plugin-stealth"
+import { loginToDiscord } from "./loginToDiscord";
+
+export const createBrowser = async () => {
+    let browser
+    try {
+        puppeteer.use(StealthPlugin());
+        browser = await puppeteer.launch({
+            executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+            userDataDir: "C:\\Users\\VG Admin\\AppData\\Local\\Google\\Chrome\\User Data\\Default",
+            headless: false,
+        })
+
+        const page = await browser.newPage();
+
+        const token = process.env.DISCORD_USER_TOKEN
+
+        if (!token) {
+            throw new Error("You need to specify a user token");
+        }
+
+        loginToDiscord(page, token)
+
+
+    } catch (err) {
+        console.log(err);
+    } finally {
+        await browser?.close()
+    }
+}
