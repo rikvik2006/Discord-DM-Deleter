f5141c41efab39bb5965c8c490de924f0e6da85a
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2023-09-20 16:48:38 +0200

    feat: Moved the mouse on top of the delete message button

--- src/services/deleteMessages.ts
+++ src/services/deleteMessages.ts
@@ -16,6 +16,8 @@ export const deleteMessages = async (page: Page) => {
         location.assign(`https://discord.com/channels/@me/${channelId}`)
     })
 
+    page.waitForSelector("ol.scrollerInner-2PPAp2 li.messageListItem-ZZ7v6g");
+
     const userMessages = await page.$$eval("ol.scrollerInner-2PPAp2 li.messageListItem-ZZ7v6g", (messages) => {
         const sameAuthMessages: HTMLElement[] = []
         for (let message of messages) {
@@ -35,13 +37,38 @@ export const deleteMessages = async (page: Page) => {
         const messageBoudingBox = await message.boundingBox()
         if (!messageBoudingBox) continue
 
-        const x = messageBoudingBox.x + messageBoudingBox.width / 2;
-        const y = messageBoudingBox.y + messageBoudingBox.height / 2
+        const messageX = messageBoudingBox.x + messageBoudingBox.width / 2;
+        const messageY = messageBoudingBox.y + messageBoudingBox.height / 2
 
-        await page.mouse.move(x, y)
+        await page.mouse.move(messageX, messageY)
         await page.keyboard.press("ShiftLeft")
         await page.keyboard.down("ShiftLeft");
 
+        const deleteButtons: HTMLDivElement[] = await message.$$eval(".buttonContainer-1502pf .buttons-3dF5Kd div.buttonsInner-1ynJCY div.button-3bklZh", (buttons) => {
+            const deleteButtons: HTMLDivElement[] = []
 
+            for (let button of buttons) {
+                const buttonClassList: string[] = Array.from(button.classList);
+
+                if (!buttonClassList.includes("dangerous-Y36ifs")) continue;
+
+                deleteButtons.push(button);
+            }
+
+            return deleteButtons
+        })
+
+        const deleteButtonsHandleElements: ElementHandle<HTMLDivElement>[] = await Promise.all(deleteButtons.map(async (deleteButton) => await page.evaluateHandle(e => e, deleteButton)))
+
+        for (let deleteButton of deleteButtonsHandleElements) {
+            const deleteButtonBoundingBox = await deleteButton.boundingBox();
+            if (!deleteButtonBoundingBox) continue;
+
+            const x = deleteButtonBoundingBox.x + deleteButtonBoundingBox.width / 2;
+            const y = deleteButtonBoundingBox.y + deleteButtonBoundingBox.height / 2;
+
+            await page.mouse.move(x, y);
+            await page.mouse.click(x, y);
+        }
     }
 }
